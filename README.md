c05bec2c1419970a2ecd2a6466194a11bb4269ce
Christophe Meyer <christophe.meyer.pro@gmail.com>
2019-09-29 13:29:28 -0400

    Add option to give log file directly

--- README.md
+++ README.md
@@ -3,6 +3,25 @@
 Trigger a sound when a text matches in Everquest.
 
 ## How to use
+
+```
+usage: main.py [-h] [--text TEXT] [-i]
+               (--char_name CHAR_NAME | --log_file LOG_FILE)
+
+Trigger a sound when some text is matched in Everquest logs.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --text TEXT           When this text appears in game, a sound is played.
+  -i                    Text match is case insensitive (ignore capitalization
+                        of words).
+  --char_name CHAR_NAME
+                        Name of your character.
+  --log_file LOG_FILE   Path to log file, e.g.: ~/Library/Application
+                        Support/EverQuest/PlayerLogseqlog_Novina_loginse.txt
+```
+
+Example:
 ```
 python main.py --char_name Novina --text "some text that you want to trigger a sound!"
 ```
--- main.py
+++ main.py
@@ -8,27 +8,26 @@ from pydub.playback import play
 
 
 SOUND_FILE = "zapsplat_emergency_alarm_siren_011_26617.mp3"
+song = AudioSegment.from_mp3(SOUND_FILE)
 LOG_PATH = Path("~/Library/Application Support/EverQuest/PlayerLogs/")
 
-
 ap = argparse.ArgumentParser(description="Trigger a sound when some text is matched in Everquest logs.")
-ap.add_argument("--char_name", type=str, default="Novina", help="Name of your character.")
 ap.add_argument("--text", type=str, default="rage", help="When this text appears in game, a sound is played.")
 ap.add_argument("-i", action="store_true", help="Text match is case insensitive (ignore capitalization of words).")
+group = ap.add_mutually_exclusive_group(required=True)
+group.add_argument("--char_name", type=str, help="Name of your character.")
+group.add_argument("--log_file", type=argparse.FileType('r'), help=f"Path to log file, e.g.: {LOG_PATH}eqlog_Novina_loginse.txt")
 args = ap.parse_args()
 
-song = AudioSegment.from_mp3(SOUND_FILE)
-log_file = os.path.expanduser(LOG_PATH / "eqlog_{}_loginse.txt".format(args.char_name))
-trigger_text = args.text
 
-with open(log_file) as file:
+def listen_loop(file, trigger_text, case_insensitive_enabled):
     # start listening from end of log file
     file.seek(0, os.SEEK_END)  # offset=0, whence="from the end"
 
     while True:
         line = file.readline()
         if line:
-            if args.i is True:
+            if case_insensitive_enabled:
                 trigger_text = trigger_text.lower()
                 line = line.lower()
 
@@ -37,3 +36,13 @@ with open(log_file) as file:
                 play(song)
         else:
             time.sleep(0.5)
+
+
+listen_loop_args = dict(trigger_text=args.text, case_insensitive_enabled=args.i)
+if args.log_file is None:
+    log_file = os.path.expanduser(LOG_PATH / "eqlog_{}_loginse.txt".format(args.char_name))
+    with open(log_file) as file:
+        listen_loop(file=file, **listen_loop_args)
+else:
+    log_file = args.log_file
+    listen_loop(file=log_file, **listen_loop_args)
